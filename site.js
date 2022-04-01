b271b72b47932023fb8a85dc1ab53215249493e7
Stephanieee-pixel <122Smorales@frhsd.com>
2022-04-01 13:29:04 -0400

    month is now displayed

--- site.js
+++ site.js
@@ -42,49 +42,46 @@ const vue_app = Vue.createApp({
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
             getMonthText(dateArray){
-              var month;
-              var releasedDate;
 switch(dateArray[1]) {
-    case '1':
+    case 1:
         month = 'January';
         break;
-    case '2':
+    case 2:
         month = 'February';
         break;
-    case '3':
+    case 3:
         month = 'March';
         break;
-    case '4':
+    case 4:
         month = 'April';
         break;
-    case '5':
+    case 5:
         month = 'May';
         break;
-    case '6':
+    case 6:
         month = 'June';
         break;
-    case '7':
+    case 7:
         month = 'July';
         break;
-    case '8':
+    case 8:
         month = 'August';
         break;
-    case '9':
+    case 9:
         month = 'September';
         break;
-    case '10':
+    case 10:
         month = 'October';
         break;
-    case '11':
+    case 11:
         month = 'November';
         break;
-    case '12':
+    case 12:
         month = 'December';
         break;
       }
-      var date= month+ " "+dateArray[2]+", "+dateArray[0];
+      return month+ " "+dateArray[2]+", "+dateArray[0];
 
-      return date;
     },
             posterClick(index){
             return this.movies[index].posterindex++;
